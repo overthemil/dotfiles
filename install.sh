756c0d47c95067f734822b74125ba5bd3014fef2
Pedro Alves <pedro.valentealves@connect.qut.edu.au>
2019-01-09 19:25:13 +1000

    Improved install script for blank Arch installs

--- install.sh
+++ install.sh
@@ -2,35 +2,40 @@
 
 FOLDER=$(pwd)
 
+sudo pacman -S xorg-server xorg-xinit xorg-utils xorg-server-utils xorg-xrdb;
+sudo pacman -S vim tmux rxvt-unicode xclip fzf cmake; 
+sudo pacman -S i3-gaps i3blocks i3status ttf-dejavu rofi ranger feh; 
+sudo pacman -S i3lock imagemagick scrot; 
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3blocks
+
 if [ -d "$FOLDER/.git" ]
 then
-    rm ~/.bashrc
+    rm -f ~/.bashrc
     ln -s $FOLDER/.bashrc ~/.bashrc    
-    rm ~/.vimrc
-	ln -s $FOLDER/.vimrc ~/.vimrc
-	rm ~/.tmux.conf
-	ln -s $FOLDER/.tmux.conf ~/.tmux.conf
-    rm ~/.Xresources 
+    rm -f ~/.vimrc
+    ln -s $FOLDER/.vimrc ~/.vimrc
+    rm -f ~/.tmux.conf
+    ln -s $FOLDER/.tmux.conf ~/.tmux.conf
+    rm -f ~/.Xresources 
     ln -s $FOLDER/.Xresources ~/.Xresources 
-    rm ~/.wallpaper 
+    rm -f ~/.wallpaper 
     ln -s $FOLDER/.wallpaper ~/.wallpaper 
-    rm -r ~/.config/i3
+    rm -f -r ~/.config/i3
     ln -s $FOLDER/config/i3 ~/.config/i3 
-    rm -r ~/.config/i3blocks 
+    rm -f -r ~/.config/i3blocks 
     ln -s $FOLDER/config/i3blocks ~/.config/i3blocks
+    rm -f ~/.xinitrc 
+    ln -s $FOLDER/.xinitrc ~/.xinitrc 
 fi
+
+git clone https://aur.archlinux.org/trizen.git;
+cd trizen
+makepkg -si;
+cd ..
+trizen -S ttf-font-awesome-4;
 
 python ~/.vim/plugged/YouCompleteMe/install.py;
 
-sudo pacman -S git;
-
-git clone https://aur.archlinux.org/trizen.git;
-cd trizen;
-makepkg -si;
-cd ..;
-rm -r trizen;
-
-sudo pacman -S tmux;
-sudo pacman -S imagemagick;
-sudo pacman -S scrot;
-trizen -S ttf-font-awesome-4;
+source ~/.bashrc;
