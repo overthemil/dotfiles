718d8f2ffd2211a544da7e451e78e3e93b9b076a
Pedro Alves <33297928+skrewby@users.noreply.github.com>
2023-09-23 19:21:30 +1000

    Add chsh to fedora packages

--- fedora_packages.sh
+++ fedora_packages.sh
@@ -6,7 +6,7 @@ echo "Installing Fedora packages  "
 echo "----------------------------"
 echo ""
 
-dnf install neovim kitty zsh util-linux clang-tools-extra gcc-c++ fzf ripgrep
+dnf install neovim kitty zsh util-linux clang-tools-extra gcc-c++ fzf ripgrep util-linux-user
 dnf -y groupinstall "Development Tools"
 dnf -y groupinstall c-development
 
